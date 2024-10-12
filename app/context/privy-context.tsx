237ea3401332adcd7ab861e71bff85679861d981
kartikay <kartikaygautam24617@gmail.com>
2024-10-12 17:31:56 +0530

    should work

--- app/context/privy-context.tsx
+++ app/context/privy-context.tsx
@@ -1,7 +1,11 @@
 "use client";
 
 import { PrivyProvider } from "@privy-io/react-auth";
-export default function Providers({ children }: { children: React.ReactNode }) {
+export default function PrivyAuthProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <PrivyProvider appId={process.env.PRIVY_APP_ID as string}>
       {children}
