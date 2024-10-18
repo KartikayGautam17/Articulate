d606fa40d542d9811d975c47c035b5e945d2b8ea
kartikay <kartikaygautam24617@gmail.com>
2024-10-18 10:10:04 +0530

    Auth Form Init

--- app/context/mantine-context.tsx
+++
@@ -1,9 +0,0 @@
-import { MantineProvider } from "@mantine/core";
-
-type MantineUIProps = {
-  children: React.ReactNode;
-};
-
-export const MantineUI: React.FC<MantineUIProps> = ({ children }) => {
-  return <MantineProvider>{children}</MantineProvider>;
-};
